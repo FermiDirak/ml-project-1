39a32823fbb631a0b39a54c063ca61bd97f6e734
bryan <manuelebryan@gmail.com>
2021-11-29 12:42:50 -0800

    epochs

--- main.py
+++ main.py
@@ -96,6 +96,10 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-test(test_dataloader, model, loss_fn)
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
 
 print("end")
